610cff0b7bdef464c3200952971695e1560d49da
Linshengyi-William <sunlins@oregonstate.edu>
2021-05-01 23:19:12 -0700

    added comment

--- test_VolumeOfCube.py
+++ test_VolumeOfCube.py
@@ -3,12 +3,15 @@ import VolumeOfCube
 
 inp =(input("Please enter a positive, non-zero integer: "))
 class TestCase_input(unittest.TestCase):
+    # Test if the input is positive
     def test_positiveInput(self):
         self.assertGreater(int(inp),0)
+    # Test if the type of the input is int
     def test_typeInput(self):
         self.assertEqual(type(int(inp)), type(1))
 
 class TestCase_result(unittest.TestCase):
+    # Test if the result is right
     def test_isResultRight(self):
         inpp=int(inp)
         self.assertEqual(VolumeOfCube.voc(inp), inpp*inpp*inpp)
